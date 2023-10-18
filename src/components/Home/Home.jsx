78e2a05f41d66d144a0936b2903730be7e95b783
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-10-18 18:50:17 +0600

    Added .env, changed website name, added firebase AUTH keys, installed firebase, added Outlet to Root.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <h2>I AM HOME</h2>
+    </>
+  );
 };
 
 export default Home;
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 const Root = () => {
-  return <></>;
+  return (
+    <>
+      <Outlet></Outlet>
+    </>
+  );
 };
 
 export default Root;
