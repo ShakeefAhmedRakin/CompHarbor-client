a6441e3fe42af9d9383c5d45c024b7ba5bdf211c
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-10-20 03:51:38 +0600

    Styled the cart page.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -40,34 +40,35 @@ const Cart = () => {
   return (
     <>
       <Toaster position="bottom-right" richColors />
-      <div className="container mx-auto px-4 py-20">
-        <h1 className="text-center font-bold text-4xl text-gray-900 dark:text-white">
-          Your Cart
-        </h1>
-        <hr className="my-5" />
-        <div className="container px-2 mx-auto my-8">
-          <div className="flex justify-center">
-            {products.length > 0 ? (
-              <div className="w-full lg:w-fit grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                {products.map((product) => (
-                  <CartCard
-                    key={product._id}
-                    product={product}
-                    handleDelete={handleDelete}
-                  ></CartCard>
-                ))}
-              </div>
-            ) : (
-              <div className="py-36 flex flex-col items-center">
-                <h1 className="text-center font-bold text-3xl text-gray-900 dark:text-white">
-                  No items added yet!
-                </h1>
-                <BsFillCartXFill className="text-6xl mt-4 text-gray-900 dark:text-white"></BsFillCartXFill>
-              </div>
-            )}
+      <div className="container mx-auto px-4 py-20 ">
+        <div className="border-[1px] bg-white dark:bg-gray-900 border-black dark:border-white">
+          <h1 className="text-center my-10 font-bold text-4xl text-gray-900 dark:text-white">
+            Your Cart
+          </h1>
+          <hr className="my-5" />
+          <div className="container px-2 mx-auto my-8">
+            <div className="flex justify-center">
+              {products.length > 0 ? (
+                <div className="w-full lg:w-fit grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                  {products.map((product) => (
+                    <CartCard
+                      key={product._id}
+                      product={product}
+                      handleDelete={handleDelete}
+                    ></CartCard>
+                  ))}
+                </div>
+              ) : (
+                <div className="py-36 flex flex-col items-center">
+                  <h1 className="text-center font-bold text-3xl text-gray-900 dark:text-white">
+                    No items added yet!
+                  </h1>
+                  <BsFillCartXFill className="text-6xl mt-4 text-gray-900 dark:text-white"></BsFillCartXFill>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-        <hr className="my-5" />
       </div>
     </>
   );
