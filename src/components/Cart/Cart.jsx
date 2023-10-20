c643b05a982d2bc5477ab1304ffe3c2bf8f171c9
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-10-21 04:27:45 +0600

    Changed the look of the cart cards.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -43,7 +43,7 @@ const Cart = () => {
       <Toaster position="bottom-right" richColors />
       <div data-aos="fade-up" className="container mx-auto px-4 py-20 ">
         <div className="border-[1px] bg-white dark:bg-gray-900 border-black dark:border-white">
-          <h1 className="bg-primaryLight dark:bg-primaryDark py-5 text-center my-5 mx-4 font-bold text-4xl text-white">
+          <h1 className="bg-primaryLight dark:bg-primaryDark py-5 text-center my-5 mx-4 font-bold text-xl md:text-4xl text-white">
             Your Cart
           </h1>
           <hr className="my-5" />
@@ -57,7 +57,7 @@ const Cart = () => {
             ) : (
               <div className="flex justify-center">
                 {products.length > 0 ? (
-                  <div className="w-full lg:w-fit grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                  <div className="w-full px-1 md:px-16 grid grid-cols-1 xl:grid-cols-2 gap-5">
                     {products.map((product) => (
                       <CartCard
                         key={product.cart_id}
@@ -68,10 +68,10 @@ const Cart = () => {
                   </div>
                 ) : (
                   <div className="py-36 flex flex-col items-center">
-                    <h1 className="text-center font-bold text-3xl text-gray-900 dark:text-white">
+                    <h1 className="text-center font-bold text-xl md:text-3xl text-gray-900 dark:text-white">
                       No items added yet!
                     </h1>
-                    <BsFillCartXFill className="text-6xl mt-4 text-gray-900 dark:text-white"></BsFillCartXFill>
+                    <BsFillCartXFill className="text-4xl md:text-5xl mt-4 text-gray-900 dark:text-white"></BsFillCartXFill>
                   </div>
                 )}
               </div>
--- src/components/CartCard/CartCard.jsx
+++ src/components/CartCard/CartCard.jsx
@@ -1,53 +1,58 @@
 import PropTypes from "prop-types";
 import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
+import { ImCross } from "react-icons/im";
 
 const CartCard = ({ product, handleDelete }) => {
   return (
     <>
-      <div className="flex flex-col justify-between w-full max-w-lg bg-gray-50 border-[1.5px] border-black dark:border-white shadow dark:bg-gray-800">
-        <img
-          className="p-8 rounded-t-lg w-full h-[200px] lg:h-[320px] object-contain"
-          src={product.product_image}
-          alt="product image"
-        />
+      <div className="flex justify-start w-full max-w-[1500px] bg-gray-50 border-[1.5px] border-black dark:border-white shadow dark:bg-gray-800">
+        <div className="border-r-[1px] border-black">
+          <img
+            className="aspect-square h-[100px] md:h-[200px] object-contain"
+            src={product.product_image}
+            alt="product image"
+          />
+        </div>
 
-        <hr />
-
-        <div className="px-5 pb-5">
-          <div className="flex justify-between">
-            <span className="badge my-4 font-bold">
-              {product.product_brand}
-            </span>
-            <span className="badge my-4 font-medium">
-              {product.product_type}
-            </span>
+        <div className="flex flex-col justify-between flex-1 p-2 md:p-4 gap-1">
+          <div className="flex flex-col">
+            <div className="flex justify-between w-full">
+              <h5 className="text-xs md:text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                {product.product_name}
+              </h5>
+              <h1 className="text-sm md:text-xl font-extrabold text-gray-900 dark:text-white">
+                {product.product_price}৳
+              </h1>
+            </div>
+            <hr className="my-2" />
+            <div className="flex gap-1">
+              <span className="badge font-bold badge-sm md:badge-md">
+                {product.product_brand}
+              </span>
+              <span className="badge font-medium badge-sm md:badge-md">
+                {product.product_type}
+              </span>
+            </div>
+            <div className="ml-1 my-2">
+              <div className="flex items-center">
+                <Rating
+                  style={{ maxWidth: 120 }}
+                  value={parseInt(product.product_rating)}
+                  readOnly
+                />
+                <span className=" bg-blue-100 dark:bg-primaryDark pt-1 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:text-white ml-3">
+                  {product.product_rating}.0
+                </span>
+              </div>
+            </div>
           </div>
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-            {product.product_name}
-          </h5>
-
-          <div className="my-2 flex justify-between">
-            <Rating
-              style={{ maxWidth: 120 }}
-              value={parseInt(product.product_rating)}
-              readOnly
-            />
-            <span className="bg-blue-100 dark:bg-primaryDark pt-1 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:text-white ml-3">
-              {product.product_rating}.0
-            </span>
-          </div>
-
-          <div className="flex justify-between items-center mt-4">
-            <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
-              {product.product_price}৳
-            </h1>
-
+          <div className="flex justify-end items-center">
             <button
               onClick={() => handleDelete(product.cart_id)}
-              className="btn border-none bg-primaryLight dark:bg-primaryDark hover:dark:bg-primaryDark text-white hover:bg-primaryLight"
+              className="btn btn-sm md:btn-md border-none bg-primaryLight dark:bg-primaryDark hover:dark:bg-primaryDark text-white hover:bg-primaryLight"
             >
-              Remove
+              <ImCross className="text-[10px] md:text-base"></ImCross>
             </button>
           </div>
         </div>
