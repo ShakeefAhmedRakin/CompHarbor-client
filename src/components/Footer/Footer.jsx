8dad5c31198936c16dd69c3f894fb40eb67cd721
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-10-20 17:52:12 +0600

    Completed the footer section and styled it differently.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,30 @@
 import { PiComputerTowerBold } from "react-icons/pi";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-      <div>
-        <footer className="footer p-10 bg-primaryLight dark:bg-gray-800 text-neutral-content flex justify-center gap-40">
-          <aside className="flex flex-col items-center">
-            <div className="px-8 py-6 bg-transparent mb-5 hover:bg-transparent hover:border-white  normal-case text-xl md:text-3xl border-[1px] border-white rounded-none">
-              <h1 className="font-normal text-white tracking-widest flex items-center gap-x-1">
-                <PiComputerTowerBold></PiComputerTowerBold>Comp Harbor
-              </h1>
-            </div>
-            <div className="text-base text-center text-white space-y-1">
-              <p>Computer Harbor Industries</p>
-              <p className="pb-2">Providing reliable tech since 2020</p>
-              <hr />
-              <p className="pt-2">
-                Copyright © 2020 - All right reserved by Computer Harbor
-                Industries Ltd
-              </p>
-            </div>
-          </aside>
-        </footer>
-      </div>
+      <footer className="footer footer-center bg-primaryLight dark:bg-primaryDark text-white py-10">
+        <div className="px-8 py-6 bg-transparent hover:bg-transparent normal-case text-xl md:text-3xl border-[1px] border-white rounded-none">
+          <h1 className="font-normal text-white tracking-widest flex items-center gap-x-1">
+            <PiComputerTowerBold></PiComputerTowerBold>Comp Harbor
+          </h1>
+        </div>
+        <nav className="grid grid-flow-col gap-4 border-white border-[1px] p-5">
+          <Link to="/">
+            <a className="link link-hover">Home</a>
+          </Link>
+          <Link to="/login">
+            <a className="link link-hover">Login</a>
+          </Link>
+          <Link to="/register">
+            <a className="link link-hover">Sign Up</a>
+          </Link>
+        </nav>
+        <aside>
+          <p>Copyright © 2023 - All right reserved by Computer Harbor Ltd</p>
+        </aside>
+      </footer>
     </>
   );
 };
